5f755ce57e31f8754dc03541cd70022ef653b334
sofi tymechko <sofiatymechko@gmail.com>
2023-11-21 12:05:52 +0200

    add logic to protect routes, add signUp, signIn and signOut logic to forms, add startups route

---
+++ src/routes/StartupsRoute.jsx
@@ -0,0 +1,11 @@
+import SignOutForm from "../components/forms/SignOutForm";
+
+const StartupsRoute = () => {
+  return (
+    <>
+      <SignOutForm />
+    </>
+  );
+};
+
+export default StartupsRoute;
