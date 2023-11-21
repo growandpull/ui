92e9b1121faa40d60c48e8d2ab84b6dda0a775bf
sofi tymechko <sofiatymechko@gmail.com>
2023-11-21 18:10:22 +0200

    fix width of layout with BigImageLogo

--- src/layouts/LogoHalfWidthLayout.jsx
+++ src/layouts/LogoHalfWidthLayout.jsx
@@ -5,14 +5,14 @@ import BigLogoImage from "../assets/BigLogoImage.svg";
 const LogoHalfWidthLayout = () => {
   return (
     <Grid container>
-      <Grid item xs={6}>
+      <Grid item xs={5}>
         <img
           src={BigLogoImage}
           alt="BigLogoImage"
           style={{ width: "100%", height: "100vh" }}
         />
       </Grid>
-      <Grid item xs={6}>
+      <Grid item xs={7}>
         <Stack justifyContent="center" alignItems="center" height={1}>
           <Outlet />
         </Stack>
