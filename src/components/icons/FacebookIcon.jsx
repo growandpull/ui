f6a5abb182fab9c2dbd05a973fbeefdf8109a284
sofi tymechko <sofiatymechko@gmail.com>
2023-11-23 00:05:13 +0200

    add icons for footer

---
+++ src/components/icons/FacebookIcon.jsx
@@ -0,0 +1,18 @@
+function FacebookIcon(props) {
+  return (
+    <svg
+      width={15}
+      height={28}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M3.84 27.696V13.848H.36V9.076l3.48-.002-.005-2.81C3.835 2.37 4.891 0 9.477 0h4.73v4.774h-3.3c-1.785 0-1.87.667-1.87 1.911L9.03 9.074h5.222l-.505 4.773-4.714.001-.003 13.848H3.84z"
+        fill="#fff"
+      />
+    </svg>
+  );
+}
+
+export default FacebookIcon;
