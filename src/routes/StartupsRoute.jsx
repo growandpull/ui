894616f9749673619504c511ed4a4376e10d8da1
sofi tymechko <sofiatymechko@gmail.com>
2023-11-28 23:13:45 +0200

    import components to startups route

--- src/routes/StartupsRoute.jsx
+++ src/routes/StartupsRoute.jsx
@@ -1,5 +1,23 @@
+import { Stack } from "@mui/material";
+import TopPageHeader from "../components/TopPageHeader";
+import Startups from "../assets/outlined-texts/sturtups.svg";
+import StartupsList from "../components/startups/StartupsList";
+import StyledTextGroup from "../components/styled/StyledTextGroup";
+
 const StartupsRoute = () => {
-  return <></>;
+  return (
+    <Stack>
+      <Stack gap={35} marginBlockEnd={50} marginBlockStart={60}>
+        <TopPageHeader />
+        <StyledTextGroup
+          imgTextSrc={Startups}
+          variant="h2"
+          text="Innovate. Invest. Inspire."
+        />
+      </Stack>
+      <StartupsList />
+    </Stack>
+  );
 };
 
 export default StartupsRoute;
